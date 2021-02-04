334fa634f842cbcf15ca8564915a9c0981c6540d
Alvarodevs <alvaro.develops@gmail.com>
2021-02-04 16:20:09 +0000

    First commit

--- exercises/09-Function-parameters/app.py
+++ exercises/09-Function-parameters/app.py
@@ -1,6 +1,7 @@
 # Your code goes here:
-def render_person(param):
-    return param
+def render_person(name, dob, eyecolor, age, gender):
+    phrase = (f'{name} is a {age} years old {gender} born in {dob} with {eyecolor} eyes')
+    return phrase
 
 
 # Do not edit below this line
